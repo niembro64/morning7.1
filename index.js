0bacf1a9c8e3de41eb0318dd582c4aebd803b20d
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-07 12:18:36 -0500

    tetmp

--- index.js
+++ index.js
@@ -12,7 +12,37 @@ doThing();
 
 // destructuring
 const faveFlavors = [
-    "one", "two", "three"
+    "one", "two", "three", "four"
 ]
 const[a,b,c] = faveFlavors;
 console.log(b);
+
+const sundae = {
+    flavor: "vanilla",
+    sauce: "hot fudge",
+    toppings : ["pecans", "rainbow", "chocolate"]
+}
+
+const {toppings} = sundae;
+toppings[0] = "peanuts";
+console.log(toppings);
+console.log(sundae);
+
+// rest and spread
+
+const[first, ...others] = faveFlavors;
+console.log(others);
+
+const newList = ["hot cheeto", ...faveFlavors];
+console.log(newList);
+
+const {flavor, ...fixings} = sundae;
+console.log(fixings);
+
+const superChocoloatey = {
+    flavor: "chocolate",
+    ...fixings
+}
+console.log(superChocoloatey);
+
+// arrow functions
